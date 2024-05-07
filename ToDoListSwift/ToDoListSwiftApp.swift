a461120470df1020d48fabee1dccf521d09117bf
zheer barzan <zheerbarzan85@outlook.com>
2024-05-07 21:42:00 +0300

    Initial Commit

---
+++ ToDoListSwift/ToDoListSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListSwiftApp.swift
+//  ToDoListSwift
+//
+//  Created by zheer barzan on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
