c360c1e72901ba8499065cb83dd93e90b01407ef
zheer barzan <zheerbarzan85@outlook.com>
2024-05-11 16:46:45 +0300

    adding the necessary folders

--- ToDoListSwift/ToDoListSwiftApp.swift
+++ ToDoListSwift/ToDoListSwiftApp.swift
@@ -4,11 +4,15 @@
 //
 //  Created by zheer barzan on 7/5/24.
 //
-
+import FirebaseCore
 import SwiftUI
 
 @main
 struct ToDoListSwiftApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
